86c78bd1cb4fefba168dae716daccec4230c3207
EllieNguyen <ellieN1235@gmail.com>
2023-10-03 20:47:58 -0700

    Initial Commit

---
+++ ReelTalkOnboarding/ReelTalkOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelTalkOnboardingApp.swift
+//  ReelTalkOnboarding
+//
+//  Created by Ellie on 10/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ReelTalkOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
